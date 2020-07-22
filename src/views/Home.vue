d800906657fd602426aa512512c8431dfca0ea2b
Juan-Garcia1 <juan_garcia_1995@hotmail.com>
2020-07-22 12:36:36 -0500

    create homepage to view list of users

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,48 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <p v-if="error">{{ errorMsg }}</p>
+    <div v-else>
+      <p v-if="loading">Loading...</p>
+
+      <ul class="user-list">
+        <li v-for="user in users" :key="user.id" class="user">
+          <router-link
+            :to="{ name: 'UserPosts', params: { userId: user.id } }"
+            >{{ user.name }}</router-link
+          >
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import axios from "axios";
 export default {
   name: "Home",
-  components: {
-    HelloWorld
-  }
+  components: {},
+  data: function() {
+    return {
+      users: [],
+      loading: true,
+      error: false,
+      errorMsg: "",
+    };
+  },
+  mounted: function() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        this.users = res.data;
+      })
+      .catch((err) => {
+        this.error = true;
+        this.errorMsg = err;
+      })
+      .finally(() => {
+        this.loading = false;
+      });
+  },
 };
 </script>
